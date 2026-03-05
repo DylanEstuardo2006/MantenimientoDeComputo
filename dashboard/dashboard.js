e76c2bc34517f80ca0acddef2848be6367ac87c4
Dylan Estuardo Flores Sagahón <dylan2006122@gmai.com>
2026-03-05 14:29:53 -0600

    Parte publica en funcionamiento, desarollo del dashboard

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,7 +1,50 @@
-function cargarVista(ruta) {
+window.cargarVista= function(ruta){
     fetch(ruta)
         .then(response => response.text())
         .then(data => {
+
             document.getElementById("contenido-principal").innerHTML = data;
+
+            if (window.innerWidth < 992) {
+                sidebar.classList.remove("show-sidebar");
+                overlay.classList.remove("show");
+            }
+
         });
 }
+// botones
+const btnMenu = document.getElementById("btnMenu");
+const sidebar = document.getElementById("sidebar");
+
+// abrir / cerrar
+btnMenu.addEventListener("click", () => {
+    sidebar.classList.toggle("show-sidebar");
+});
+
+// cerrar si hacen click fuera del menú
+document.addEventListener("click", (e) => {
+
+    const clickDentroSidebar = sidebar.contains(e.target);
+    const clickBoton = btnMenu.contains(e.target);
+
+    if (!clickDentroSidebar && !clickBoton) {
+        sidebar.classList.remove("show-sidebar");
+    }
+
+});
+
+const overlay = document.getElementById("overlay");
+
+// cerrar al presionar overlay
+overlay.addEventListener("click", () => {
+    sidebar.classList.remove("show-sidebar");
+    overlay.classList.remove("show");
+});
+
+// limpiar cuando cambia el tamaño de pantalla
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 992) {
+        sidebar.classList.remove("show-sidebar");
+        overlay.classList.remove("show");
+    }
+});
